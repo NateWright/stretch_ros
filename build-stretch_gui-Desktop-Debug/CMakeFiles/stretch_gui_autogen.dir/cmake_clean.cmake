ed680746df8107b28bd9ff7971119011fccf6787
nwright <nathanwrightbusiness@gmail.com>
2022-05-10 12:16:26 -0400

    UI imported from Patrick Repo

--- build-stretch_gui-Desktop-Debug/CMakeFiles/stretch_gui_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/stretch_gui_autogen"
-  "stretch_gui_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/stretch_gui_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
