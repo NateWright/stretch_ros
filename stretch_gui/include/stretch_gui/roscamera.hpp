c73925d883b94cc39322241af6f8b8acd2929f8a
nwright <nathanwrightbusiness@gmail.com>
2022-05-16 17:28:58 -0400

    renamed class to follow better naming

--- stretch_gui/include/stretch_gui/roscamera.hpp
+++ stretch_gui/include/stretch_gui/roscamera.hpp
@@ -1,32 +1,37 @@
 #ifndef ROSCAMERA_HPP
 #define ROSCAMERA_HPP
 
-#include <QThread>
-#include <QObject>
-#include <QWidget>
-#include <QPixmap>
-#include <QImage>
-#include <QColor>
-#include <sensor_msgs/PointCloud2.h>
-#include <ros/ros.h>
+#include <geometry_msgs/PointStamped.h>
 #include <pcl_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <stretch_moveit_grasps/stretch_move_bool.h>
 
-enum direction{Up, Down, Left, Right, Home};
+#include <QColor>
+#include <QImage>
+#include <QObject>
+#include <QPixmap>
+#include <QThread>
+#include <QWidget>
 
-class rosCamera : public QThread
-{
-  Q_OBJECT
-  public:
-    explicit rosCamera(ros::NodeHandle *nh);
-    ~rosCamera();
+enum direction { Up, Down, Left, Right, Home };
+
+class RosCamera : public QThread {
+    Q_OBJECT
+   public:
+    explicit RosCamera(ros::NodeHandle *nh);
+    ~RosCamera();
     void run() override;
 
-  private:
+   private:
     ros::NodeHandle *nh_;
     ros::Subscriber colorCameraSub_;
     ros::Subscriber segmentedCameraSub_;
     ros::Publisher cameraAdjustment_;
+    ros::Publisher pointPick_;
+
+    std::string frameId_;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
 
     QImage camera_;
     QPixmap cameraOutput_;
@@ -35,17 +40,15 @@ class rosCamera : public QThread
     void cameraCallback(const sensor_msgs::PointCloud2 pc);
     void move(direction d);
 
-  signals:
+   signals:
     void imgUpdate(const QPixmap &);
-  public slots:
+   public slots:
     void moveUp();
     void moveDown();
     void moveLeft();
     void moveRight();
     void moveHome();
     void sceneClicked(int x, int y, int width, int height);
-
-
 };
 
-#endif // ROSCAMERA_HPP
+#endif  // ROSCAMERA_HPP
--- stretch_gui/src/roscamera.cpp
+++ stretch_gui/src/roscamera.cpp
@@ -1,15 +1,17 @@
 #include "roscamera.hpp"
 
-rosCamera::rosCamera(ros::NodeHandle *nh): nh_(nh){
-  colorCameraSub_ = nh_->subscribe("/camera/depth/color/points", 30, &rosCamera::cameraCallback, this);
-  cameraAdjustment_ = nh_->advertise<stretch_moveit_grasps::stretch_move_bool>("/move_head", 30);
-
+RosCamera::RosCamera(ros::NodeHandle *nh) : nh_(nh) {
+    colorCameraSub_ = nh_->subscribe("/camera/depth/color/points", 30,
+                                     &RosCamera::cameraCallback, this);
+    cameraAdjustment_ =
+        nh_->advertise<stretch_moveit_grasps::stretch_move_bool>("/move_head",
+                                                                 30);
+    pointPick_ =
+        nh->advertise<geometry_msgs::PointStamped>("/clicked_point", 30);
 }
-rosCamera::~rosCamera(){
+RosCamera::~RosCamera() {}
 
-}
-
-void rosCamera::run() {
+void RosCamera::run() {
     ros::Rate loop_rate(20);
     while (ros::ok()) {
         ros::spinOnce();
@@ -19,66 +21,67 @@ void rosCamera::run() {
     }
 }
 
-void rosCamera::cameraCallback(const sensor_msgs::PointCloud2 pc){
-  camera_ = QImage(pc.width, pc.height, QImage::Format_RGB888);
+void RosCamera::cameraCallback(const sensor_msgs::PointCloud2 pc) {
+    frameId_ = pc.header.frame_id;
+    camera_ = QImage(pc.width, pc.height, QImage::Format_RGB888);
 
-  pcl::PCLPointCloud2 pcl_pc2;
-  pcl_conversions::toPCL(pc, pcl_pc2);
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(pc, pcl_pc2);
 
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
-  pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
+    cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>());
+    pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
 
-  pcl::PointXYZRGB point;
-  for(int y = 0; y < pc.height; y++){
-    for(int x = 0; x < pc.width; x++){
-        point = cloud.get()->at(x,y);
-      camera_.setPixel(x,y, QColor(point.r, point.g, point.b).rgb());
+    pcl::PointXYZRGB point;
+    for (int y = 0; y < pc.height; y++) {
+        for (int x = 0; x < pc.width; x++) {
+            point = cloud.get()->at(x, y);
+            camera_.setPixel(x, y, QColor(point.r, point.g, point.b).rgb());
+        }
     }
-  }
-  cameraOutput_ = QPixmap::fromImage(camera_);
-  cameraOutputRotated_= cameraOutput_.transformed(QTransform().rotate(90));
-
+    cameraOutput_ = QPixmap::fromImage(camera_);
+    cameraOutputRotated_ = cameraOutput_.transformed(QTransform().rotate(90));
 }
 
-void rosCamera::move(direction d){
-  stretch_moveit_grasps::stretch_move_bool robot;
-  switch(d){
-    case Up:
-      robot.lookUp = true;
-    break;
-  case Down:
-    robot.lookDown = true;
-    break;
-  case Left:
-    robot.lookLeft = true;
-    break;
-  case Right:
-    robot.lookRight = true;
-  case Home:
-    robot.home = true;
-  }
-  robot.step = 5;
-  cameraAdjustment_.publish(robot);
-
+void RosCamera::move(direction d) {
+    stretch_moveit_grasps::stretch_move_bool robot;
+    switch (d) {
+        case Up:
+            robot.lookUp = true;
+            break;
+        case Down:
+            robot.lookDown = true;
+            break;
+        case Left:
+            robot.lookLeft = true;
+            break;
+        case Right:
+            robot.lookRight = true;
+        case Home:
+            robot.home = true;
+    }
+    robot.step = 5;
+    cameraAdjustment_.publish(robot);
 }
 
-void rosCamera::moveUp(){
-  move(Up);
-}
-void rosCamera::moveDown(){
-  move(Down);
-}
-void rosCamera::moveLeft(){
-  move(Left);
-}
-void rosCamera::moveRight(){
-  move(Right);
-}
+void RosCamera::moveUp() { move(Up); }
+void RosCamera::moveDown() { move(Down); }
+void RosCamera::moveLeft() { move(Left); }
+void RosCamera::moveRight() { move(Right); }
 
-void rosCamera::moveHome(){
-  move(Home);
-}
+void RosCamera::moveHome() { move(Home); }
 
-void rosCamera::sceneClicked(int x, int y, int width, int height){
+void RosCamera::sceneClicked(int x, int y, int width, int height) {
+    int locX = (double)x * (double)camera_.width() / (double)width;
+    int locY = (double)x * (double)camera_.height() / (double)height;
 
+    geometry_msgs::PointStamped point;
+    point.header.frame_id = frameId_;
+
+    pcl::PointXYZRGB p = cloud.get()->at(locX, locY);
+
+    point.point.x = p.x;
+    point.point.y = p.y;
+    point.point.z = p.z;
+
+    pointPick_.publish(point);
 }
