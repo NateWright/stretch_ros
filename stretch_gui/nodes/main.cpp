424df4d185da0939dcbfe0eb1a9b1cfbf17a2af9
nwright <nathanwrightbusiness@gmail.com>
2022-05-24 12:48:45 -0400

    Changed name of ros node

--- stretch_gui/nodes/main.cpp
+++ stretch_gui/nodes/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "pointcloud_translation");
+    ros::init(argc, argv, "stretch_gui");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
