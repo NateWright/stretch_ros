55a6ddbfe2add9d6833c7f9d6016d6ec41aa8db7
nwright <nathanwrightbusiness@gmail.com>
2022-06-23 14:52:01 -0400

    formatting

--- stretch_gui/nodes/main.cpp
+++ stretch_gui/nodes/main.cpp
@@ -1,9 +1,8 @@
-#include "mainwindow.hpp"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+#include "mainwindow.hpp"
+
+int main(int argc, char *argv[]) {
     ros::init(argc, argv, "stretch_gui");
     QApplication a(argc, argv);
     MainWindow w;
