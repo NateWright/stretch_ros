33a6ba79a164a47df3a55cb28063a9c2dc512782
nwright <nathanwrightbusiness@gmail.com>
2022-07-05 15:08:16 -0400

    Displays port and ip of server

--- stretch_gui_server/nodes/server_node.cpp
+++ stretch_gui_server/nodes/server_node.cpp
@@ -1,4 +1,9 @@
 #include <QApplication>
+#include <QHostAddress>
+#include <QHostInfo>
+#include <QList>
+#include <QNetworkInterface>
+#include <QString>
 #include <csignal>
 
 #include "Server.hpp"
@@ -17,7 +22,20 @@ int main(int argc, char *argv[]) {
     Server server;
 
     // QRemoteObjectHost srcNode(QUrl(QStringLiteral("local:switch")));
-    QRemoteObjectHost srcNode(QUrl(QStringLiteral("tcp://192.168.86.157:9999")));
+    // https://www.codegrepper.com/code-examples/whatever/qt+get+local+ip+address
+    QString localhostname = QHostInfo::localHostName();
+    QString ip = "tcp://";
+    QList<QHostAddress> hostList = QNetworkInterface::allAddresses();
+    for (const QHostAddress &address : hostList) {
+        if (address.protocol() == QAbstractSocket::IPv4Protocol && address.isLoopback() == false) {
+            ip += address.toString();
+            break;
+        }
+    }
+    qDebug() << "Localhost name: " << localhostname;
+    ip += ":9999";
+    qDebug() << "IP = " << ip;
+    QRemoteObjectHost srcNode(QUrl(ip), nullptr);
     srcNode.enableRemoting(&server);
     qDebug() << "start";
 
